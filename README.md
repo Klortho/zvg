67d978a9cb1d458f917469718010838f84f05350
Chris Maloney <voldrani@gmail.com>
2016-04-10 00:40:25 -0400

    got it working in jsfiddle

--- README.md
+++ README.md
@@ -1,30 +1,37 @@
 # Zombo Vector Graphics
 
-Playing around with Ramda and Raphael.
+Playing around with Ramda and Raphael, to make a more functional-programming
+style library for SVG.
 
-Here's the [demo](https://nicolausmaloney.org/zvg/).
+See the [demo](https://nicolausmaloney.org/zvg/) (not much yet).
 
-To include this in a jsfiddle, add this as an
+To include this in a jsfiddle, add this as an external resource:
+
+    https://nicolausmaloney.org/zvg/zvg-bundle.js
+
+Or just fork [this one](https://jsfiddle.net/klortho/mr3omt5b/1/).
+
+
+## Development
+
+*[This is very primitive now. I would like to use webpack for it.]*
 
 Setup (do this once): `scripts/setup.sh`.
 
-Build dist: `scripts/make.pl`.
-
-See the [demo](https://zombom.github.io/zvg).
+Build the dist: `scripts/make.pl`.
 
 This also does an automatic deploy to the server whenever there's a
 commit to master.
 
-To publish manually:
+To deploy manually:
 
 ```
 scripts/make.pl
-scripts/publish.sh
+scripts/deploy.sh
 ```
 
 ## To do
 
-* Add to the publish hook a command to purge the cloudflare cache
+* Add, to the deploy hook, a command to purge the cloudflare cache
   ([described
   here](https://www.cloudflare.com/a/caching/nicolausmaloney.org#purge_cache))
-  
--- src/zvg.js
+++ src/zvg.js
@@ -1,22 +1,61 @@
+// This defines a lot of globals, as properties on the window object,
+// with accessor functions. This way, we're sure that we won't instantiate
+// the Raphael paper object too soon.
 
 (function() {
-  var paper = window.paper = new Raphael('paper');
+  var ZVG = {
+    paper: () => new Raphael('paper'),
 
-  pp = p => `${p[0]} ${p[1]} `;
-  cmd = ltr => R.concat(ltr + ' ');
-  M = cmd('M');
-  L = cmd('L');
-  Z = 'Z';
+    pp: function() {
+      return p => `${p[0]} ${p[1]} `;
+    },
 
-  triangle = function(color, v0, v1, v2) {
-    var path = M(pp(v0)) + 
-      L(pp(v1)) + 
-      L(pp(v2)) + 
-      Z;
-    var c = paper.path(path)
-      .attr({
-        fill: color
-      });
+    cmd: function() {
+      return ltr => R.concat(ltr + ' ');
+    },
+
+    M: function() {
+      return cmd('M');
+    },
+
+    L: function() {
+      return cmd('L');
+    },
+
+    Z: function() {
+      return 'Z';
+    },
+
+    triangle: function() {
+      return function(color, v0, v1, v2) {
+        var path = M(pp(v0)) + 
+          L(pp(v1)) + 
+          L(pp(v2)) + 
+          Z;
+        var c = paper.path(path)
+          .attr({
+            fill: color
+          });
+      };
+    },
+  };
+
+
+  var _cache = {};
+  var propDesc = function(name) {
+    return {
+      enumerable: true,
+      get: function() {
+        if (typeof _cache[name] !== 'undefined') {
+          return _cache[name];
+        }
+        var v = _cache[name] = ZVG[name]();
+        return v;
+      },
+    };
   }
+  Object.keys(ZVG).map(name => {
+    Object.defineProperty(window, name, propDesc(name));
+  });
 })();
 
